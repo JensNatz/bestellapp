66573192808660e8cb3d34d3ce7a04d3d216e58f
Jens Natzschka <jens.natzschka@posteo.de>
2024-07-01 13:12:15 +0200

    added add to basket function

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-let arrBasket = [];
+let arrBasket = new Map();
 
 function renderDishes(){
     let container = document.getElementById('dishes-list');
@@ -22,11 +22,36 @@ function generateDishcardHTML(i){
     `;
 }
 
-function addDishToBasket(i){
-    if(1 == 1){
-        arrBasket.push([i,1]);
+
+function renderBasketList(){
+    let basketListContainer = document.getElementById('basket-entrylist');
+    basketListContainer.innerHTML = '';
+    for (let [dishId, amount] of arrBasket) {
+        basketListContainer.innerHTML += generateBasketEntryHTML(dishId, amount);
     }
 }
+
+function generateBasketEntryHTML(dishId, amount){
+    return `<div class="basket-entry">
+                <span class="entry-amount">${amount}</span>
+                <span class="entry-dishname">${dishesDB[dishId].name}</span>
+                <span class="entry-totalprice">${calcTotalEntryPrice(dishId, amount)}</span>
+            </div>`;
+}
+
+function calcTotalEntryPrice(dishId, amount){
+    return `$$$`;
+}
+
+function addDishToBasket(i){
+    if (arrBasket.has(i)){
+        let currentValue = arrBasket.get(i);
+        arrBasket.set(i, currentValue + 1);
+    } else {
+        arrBasket.set(i, 1);
+    }
+    renderBasketList();
+};
 
 function setup() {
     renderDishes()
